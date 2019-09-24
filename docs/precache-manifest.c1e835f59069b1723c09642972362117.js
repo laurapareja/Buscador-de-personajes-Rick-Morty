c3667ee041a7d857b0492a169b996f925461586f
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-09-24 09:10:23 +0200

    updating doc

---
+++ docs/precache-manifest.c1e835f59069b1723c09642972362117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d81d2caa2c72a5538dc7caa7b4885e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebf30fbe5f64779c035c",
+    "url": "./static/css/main.82faa98c.chunk.css"
+  },
+  {
+    "revision": "4cff899bfdb55c8949a9",
+    "url": "./static/js/2.9916559a.chunk.js"
+  },
+  {
+    "revision": "ebf30fbe5f64779c035c",
+    "url": "./static/js/main.4015ad01.chunk.js"
+  },
+  {
+    "revision": "c6e887b73e5f533a009b",
+    "url": "./static/js/runtime-main.d2c7f5ed.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
